d904bab614553e8fa162b6f82dd14188e6be9961
Osmery <osmeryg74@gmail.com>
2025-01-05 20:00:27 -0300

    aplicar estilos al home

--- src/Componentes/Home/Home.jsx
+++ src/Componentes/Home/Home.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import "./Home.css"
-import vegetalesImage from '../assets/gourmet.jpg'; // Asegúrate de que la ruta sea correcta
+import vegetalesImage from '../assets/vegetales.jpg'; // Asegúrate de que la ruta sea correcta
 
 const Home = ({ setSection, setShowModal }) => {
     return (
-        <div className="home" style={{ backgroundImage: `url(${vegetalesImage})` }}>
+        <div className="home" style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.6), rgba(255, 255, 255, 0)), url(${vegetalesImage})` }}>
             <h2>Bienvenido a la app de recetas</h2>
             <button onClick={() => setShowModal(true)}>Crear Receta</button>
             <button onClick={() => setSection("historial")}>Ver Historial</button>
